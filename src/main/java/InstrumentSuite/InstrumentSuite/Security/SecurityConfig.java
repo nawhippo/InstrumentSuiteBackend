0f6ce9b934e30baf6b6a3b9c6da6f0a474141b19
Nathan Whippo <93534375+nawhippo@users.noreply.github.com>
2024-03-14 17:25:45 -0400

    fixed docker

--- src/main/java/InstrumentSuite/InstrumentSuite/Security/SecurityConfig.java
+++ src/main/java/InstrumentSuite/InstrumentSuite/Security/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://instrumentsuite-frontend.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowCredentials(true);
